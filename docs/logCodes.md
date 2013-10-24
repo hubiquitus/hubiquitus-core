83be61bc9d7b91c39e1df030f9e423e093dbb89d
etissieres <etienne.tissieres@gmail.com>
2013-10-24 10:52:46 +0200

    improve logger & add some logs & code cleanup

---
+++ docs/logCodes.md
@@ -0,0 +1,4 @@
+hub-1   hubiquitus
+hub-2   hubiquitus
+hub-3   hubiquitus
+hub-100 adapters/inproc
--- samples/pingpong.js
+++ samples/pingpong.js
@@ -5,17 +5,19 @@
 var hubiquitus = require(__dirname + "/../lib/hubiquitus");
 var logger = require(__dirname + "/../lib/logger");
 
+logger.level = "debug";
+
 hubiquitus
   .addActor("ping", function (message) {
     logger.info(this.id + "> from " + message.publisher + " : " + message.payload);
     setTimeout((function () {
-      this.send("pong", {payload: "ping"});
+      this.send(message.publisher, {payload: "ping"});
     }).bind(this), 500);
   })
   .addActor("pong", function (message) {
     logger.info(this.id + "> from " + message.publisher + " : " + message.payload);
     setTimeout((function () {
-      this.send("ping", {payload: "pong"});
+      this.send(message.publisher, {payload: "pong"});
     }).bind(this), 500);
   })
   .send("pong", "ping", {payload: "pong"});
