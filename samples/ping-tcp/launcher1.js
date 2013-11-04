fb896b4c19ef5f0783dce95e6ec24041ffeb0eaf
etissieres <etienne.tissieres@gmail.com>
2013-11-04 12:24:44 +0100

    remove useless security & tcp sample

---
+++ samples/ping-tcp/launcher1.js
@@ -0,0 +1,14 @@
+/**
+ * @module ping-ipc launcher 1
+ */
+
+var hubiquitus = require(__dirname + "/../../lib/hubiquitus");
+var logger = require(__dirname + "/../../lib/logger");
+var utils = {
+  ip: require(__dirname + "../../utils/ip")
+};
+
+logger.level = "debug";
+
+hubiquitus.start({discoveryAddr: "epgm://" + utils.ip.resolve() + ";224.0.0.1:5555"})
+  .addActor("ping", require("./player")());
---
+++ samples/ping-tcp/launcher2.js
@@ -0,0 +1,15 @@
+/**
+ * @module ping-ipc launcher 2
+ */
+
+var hubiquitus = require(__dirname + "/../../lib/hubiquitus");
+var logger = require(__dirname + "/../../lib/logger");
+var utils = {
+  ip: require(__dirname + "../../utils/ip")
+};
+
+logger.level = "debug";
+
+hubiquitus.start({discoveryAddr: "epgm://" + utils.ip.resolve() + ";224.0.0.1:5555"})
+  .addActor("pong", require("./../ping/player")())
+  .send("pong", "ping", "ping");
