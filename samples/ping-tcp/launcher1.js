6672d6e95b051ff6289be06389c242064721e97e
etissieres <etienne.tissieres@gmail.com>
2013-11-04 12:26:34 +0100

    minor code fix

--- samples/ping-tcp/launcher1.js
+++ samples/ping-tcp/launcher1.js
@@ -5,7 +5,7 @@
 var hubiquitus = require(__dirname + "/../../lib/hubiquitus");
 var logger = require(__dirname + "/../../lib/logger");
 var utils = {
-  ip: require(__dirname + "../../utils/ip")
+  ip: require(__dirname + "/../../lib/utils/ip")
 };
 
 logger.level = "debug";
