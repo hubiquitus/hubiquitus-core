206691f0b5a1a1782fb388a8daa102562aa2c30f
etissieres <etienne.tissieres@gmail.com>
2013-11-06 18:42:07 +0100

    remote adapter uses nodejs sockets

--- samples/ping-remote/launcher1.js
+++ samples/ping-remote/launcher1.js
@@ -8,8 +8,8 @@ var logger = require(__dirname + "/../../lib/logger");
 logger.level = "info";
 
 //hubiquitus.set("discoveryAddrs", ["udp://192.168.0.50:4445"]);
-hubiquitus.start({discoveryAddr: "udp://224.0.0.1:5555"})
 //hubiquitus.start({discoveryPort: 4444})
+hubiquitus.start({discoveryAddr: "udp://224.0.0.1:5555"})
   .addActor("ping", require("./player")());
 
 
--- samples/ping-remote/launcher2.js
+++ samples/ping-remote/launcher2.js
@@ -7,9 +7,9 @@ var logger = require(__dirname + "/../../lib/logger");
 
 logger.level = "info";
 
-hubiquitus.set("discoveryAddrs", ["udp://192.168.0.50:4444"]);
-hubiquitus.start({discoveryAddr: "udp://224.0.0.1:5555"})
+//hubiquitus.set("discoveryAddrs", ["udp://192.168.0.50:4444"]);
 //hubiquitus.start({discoveryPort: 4445})
+hubiquitus.start({discoveryAddr: "udp://224.0.0.1:5555"})
   .addActor("pong", require("./player")())
   .send("pong", "ping", "ping");
 
