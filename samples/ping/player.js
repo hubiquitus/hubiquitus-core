a4b6c00cf9c65e7ba1673b3ddbad95164931ed3a
etissieres <etienne.tissieres@gmail.com>
2013-11-07 12:29:55 +0100

    jshint compliant P2

--- samples/ping/player.js
+++ samples/ping/player.js
@@ -8,7 +8,7 @@ module.exports = function () {
   var count = 0;
 
   return function (from, content) {
-    logger.info("[" + this.id + "] " + content + " from " + from + " (" + ++count + " total)");
+    logger.info("[" + this.id + "] " + content + " from " + from + " (" + (++count) + " total)");
     setTimeout((function () {
       this.send(from, "ping");
     }).bind(this), parseInt(Math.random() * 490) + 10);
