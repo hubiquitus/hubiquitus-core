80efa99b037e4ca285954f0feeff7f39e8a3a8c5
etissieres <etienne.tissieres@gmail.com>
2014-04-30 15:13:43 +0200

    complete actors module unit testing

--- test/actors_cache.js
+++ test/actors_cache.js
@@ -19,73 +19,98 @@ describe('actors & cache modules', function () {
     app.stop(done);
   });
 
-  describe('add & remove actor', function () {
+  it('should add actor to actors and to cache', function (done) {
+    var actorAdded = 0;
+    var cacheActorAdded = 0;
 
-    it('should add actor to actors and to cache', function (done) {
-      var actorAdded = 0;
-      var cacheActorAdded = 0;
+    function cacheActorAddedListener(aid, cid) {
+      cacheActorAdded++;
 
-      function cacheActorAddedListener(aid, cid) {
-        cacheActorAdded++;
+      should.exist(aid);
+      aid.should.have.type('string', 'aid should be a string');
+      should.exist(cid);
+      aid.should.have.type('string', 'cid should be a string');
+      cid.should.be.eql(properties.container.ID);
+    }
 
-        should.exist(aid);
-        aid.should.have.type('string', 'aid should be a string');
-        should.exist(cid);
-        aid.should.have.type('string', 'cid should be a string');
-        cid.should.be.eql(properties.container.ID);
-      }
+    function actorAddedListener(aid) {
+      actorAdded++;
 
-      function actorAddedListener(aid) {
-        actorAdded++;
+      should.exist(aid);
+      aid.should.have.type('string', 'aid should be a string');
 
-        should.exist(aid);
-        aid.should.have.type('string', 'aid should be a string');
+      cacheActorAdded.should.be.eql(1);
+      actorAdded.should.be.eql(1);
+      cache.removeListener('actor added', cacheActorAddedListener);
+      actors.removeListener('actor added', actorAddedListener);
+      done();
+    }
 
-        cacheActorAdded.should.be.eql(1);
-        actorAdded.should.be.eql(1);
-        cache.removeListener('actor added', cacheActorAddedListener);
-        actors.removeListener('actor added', actorAddedListener);
-        done();
-      }
+    cache.on('actor added', cacheActorAddedListener);
+    actors.on('actor added', actorAddedListener);
 
-      cache.on('actor added', cacheActorAddedListener);
-      actors.on('actor added', actorAddedListener);
+    actors.add({id: 'sample'});
+  });
 
-      actors.add({id: 'sample'});
-    });
+  it('should remove actor from actors and from cache', function (done) {
+    var actorRemoved = 0;
+    var cacheActorRemoved = 0;
 
-    it('should remove actor from actors and from cache', function (done) {
-      var actorRemoved = 0;
-      var cacheActorRemoved = 0;
+    function cacheActorRemovedListener(aid, cid) {
+      cacheActorRemoved++;
 
-      function cacheActorRemovedListener(aid, cid) {
-        cacheActorRemoved++;
+      should.exist(aid);
+      aid.should.have.type('string', 'aid should be a string');
+      should.exist(cid);
+      aid.should.have.type('string', 'cid should be a string');
+      cid.should.be.eql(properties.container.ID);
+    }
 
-        should.exist(aid);
-        aid.should.have.type('string', 'aid should be a string');
-        should.exist(cid);
-        aid.should.have.type('string', 'cid should be a string');
-        cid.should.be.eql(properties.container.ID);
-      }
+    function actorRemovedListener(aid) {
+      actorRemoved++;
 
-      function actorRemovedListener(aid) {
-        actorRemoved++;
+      should.exist(aid);
+      aid.should.have.type('string', 'aid should be a string');
 
-        should.exist(aid);
-        aid.should.have.type('string', 'aid should be a string');
+      cacheActorRemoved.should.be.eql(1);
+      actorRemoved.should.be.eql(1);
+      cache.removeListener('actor removed', cacheActorRemovedListener);
+      actors.removeListener('actor removed', actorRemovedListener);
+      done();
+    }
 
-        cacheActorRemoved.should.be.eql(1);
-        actorRemoved.should.be.eql(1);
-        cache.removeListener('actor removed', cacheActorRemovedListener);
-        actors.removeListener('actor removed', actorRemovedListener);
-        done();
-      }
+    cache.on('actor removed', cacheActorRemovedListener);
+    actors.on('actor removed', actorRemovedListener);
 
-      cache.on('actor removed', cacheActorRemovedListener);
-      actors.on('actor removed', actorRemovedListener);
+    actors.add({id: 'sample'});
+    actors.remove('sample');
+  });
 
-      actors.add({id: 'sample'});
-      actors.remove('sample');
-    });
+  it('should ensure an actor exists', function (done) {
+    actors.add({id: 'sample'});
+    var result = actors.exists('sample');
+    should.exist(result);
+    result.should.be.eql(true);
+    done();
+  });
+
+  it('should get an actor from actors', function (done) {
+    var actor = {id: 'sample'};
+    actors.add(actor);
+    var retreivedActor = actors.get('sample');
+    should.exist(retreivedActor);
+    retreivedActor.should.be.eql(actor);
+    done();
+  });
+
+  it('should get all added actors id', function (done) {
+    actors.add({id: 'sample'});
+    actors.add({id: 'fake'});
+    var aids = actors.all(actors);
+    should.exist(aids);
+    aids.should.be.instanceOf(Array);
+    aids.should.containEql('sample');
+    aids.should.containEql('fake');
+    done();
   });
 });
